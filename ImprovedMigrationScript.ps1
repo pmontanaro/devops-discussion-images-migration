ca0c683d4c5dcc2fdef59d43dff0aa46ed4cc34e
Peter Montanaro <pmontanaro@deloitte.com.au>
2022-07-15 17:49:44 +1000

    Update for multi images per comment

--- ImprovedMigrationScript.ps1
+++ ImprovedMigrationScript.ps1
@@ -9,7 +9,6 @@ function generateBaseUrl ($organisation, $project) {
     return "https://dev.azure.com/$organisation/$project"
 }
 
-
 # Source Credentials
 $sourceToken = "<SourcePAT>"
 $sourceOrg = "<SourceOrg>"
@@ -39,9 +38,9 @@ $csv = Import-Csv -Path $csvName
 foreach ($ticket in $csv) {
     $oldId = $ticket.sourceId
     $newId = $ticket.targetId
-    write-host "Checking Workitem $oldId..."
+    write-host "Checking Workitem $oldId..." -ForegroundColor Yellow
     $sourceComments = Invoke-RestMethod -Method GET -Headers $sourceBase64AuthInfo "$sourceBaseUrl/_apis/wit/workItems/$oldId/comments?api-version=6.0-preview.3"
-    
+
     $count = 0
     foreach ($c in $sourceComments.comments) {
         $comment = $c.text
@@ -49,38 +48,56 @@ foreach ($ticket in $csv) {
         # $comment -match $regex; 
         if ($comment -match $regex) {
             Write-Host "Image found!" -ForegroundColor Green
-            $oldImageUrl = $Matches[0] | % { $_.split('"')[1] }
-            Write-Host "Saving Image from comment $count..."
-            $filename = "$oldId-image-$count.jpg"
-            Invoke-RestMethod -Method GET -Headers $sourceBase64AuthInfo $oldImageUrl -OutFile "$filename"
 
-            # Begin Image Upload
+            # Create empty hash for found image URLs
+            $imageUrls = @{}
+            
+            # Go through image(s)
+            $foundImageTags = Select-String $regex -input  $comment -AllMatches | % { $_.matches }
+            
+            # Image counter incase there are multiple images per comment
+            $imgCount = 1
+            foreach ($image in $foundImageTags) {
+                $oldImageUrl = $image.Value | % { $_.split('"')[1] }
+                Write-Host "Saving Image from comment $count..."
+                $filename = "$oldId-image-$count-$imgCount.jpg"
+                Invoke-RestMethod -Method GET -Headers $sourceBase64AuthInfo $oldImageUrl -OutFile "$filename"
 
-            $filePath = Get-ChildItem -Path $filename | % { $_.FullName }
+                $filePath = Get-ChildItem -Path $filename | % { $_.FullName }
 
-            $createAttachmentUrlTemplate = "$targetBaseUrl/_apis/wit/attachments?fileName={fileName}&api-version=5.0"
-
-            $bytes = [System.IO.File]::ReadAllBytes("$filePath")
-
-            $createAttachmentUrl = $createAttachmentUrlTemplate -replace "{filename}", $filename
-
-            # Post Attachment to Target Tenant
-            $resAtt = Invoke-RestMethod -Uri $createAttachmentUrl -Method Post -ContentType "application/json" -Headers $targetBase64AuthInfo  -Body $bytes 
+                $createAttachmentUrlTemplate = "$targetBaseUrl/_apis/wit/attachments?fileName={fileName}&api-version=5.0"
+    
+                $bytes = [System.IO.File]::ReadAllBytes("$filePath")
+    
+                $createAttachmentUrl = $createAttachmentUrlTemplate -replace "{filename}", $filename
+    
+                # Post Attachment to Target Tenant
+                $resAtt = Invoke-RestMethod -Uri $createAttachmentUrl -Method Post -ContentType "application/json" -Headers $targetBase64AuthInfo  -Body $bytes 
+                $imageUrls.Add($oldImageUrl, $resAtt.url)
+                $imgCount++
+            }
 
             # Add Comment to Target Tenant
-            $newImageUrl = $resAtt.url
-            $newComment = $comment.replace("src=`"$oldImageUrl`"", "src=`"$newImageUrl`"")
-            $commentAuthor = $c.createdBy.displayName
+            foreach ($key in $imageUrls.GetEnumerator()) {
+                $oldImageUrl = $key.Name
+                $newImageUrl = $key.Value
+
+                $newComment = $comment.replace("src=`"$oldImageUrl`"", "src=`"$newImageUrl`"")
+                $commentAuthor = $c.createdBy.displayName
+                $comment = $newComment
+            }
+
             
             # Create Body
             $body = @{
-                "text" = "Comment Images Migration: Original Author: $commentAuthor </br> </br> $newComment"
+                "text" = "Comment Images Migration: </br> Original Author: $commentAuthor </br> </br> $newComment"
             }
             
             $b = $body | ConvertTo-Json
 
             Write-Host "Writing new test comment..."
             Invoke-RestMethod -Method POST -Headers $targetBase64AuthInfo -Body ($b) "$targetBaseUrl/_apis/wit/workItems/$newId/comments?api-version=6.0-preview.3" -ContentType "application/json"
+
         }
         $count++
     }
